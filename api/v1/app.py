192e5f359376026624899ad5f8225a0743dcb35f
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 08:57:36 +0100

    Add initial aoi

---
+++ api/v1/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""Create flask app"""
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+if __name__ == "__main__":
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv())
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+""""Create flask app"""
+from api.v1.views import app_views
