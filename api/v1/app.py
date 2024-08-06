1c3b49bb88ec2e662f7c96b0016e24f69af7c9de
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 09:58:17 +0100

    Func for not_found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """"Create flask app"""
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Close storage session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404"""
+    response = {"error": "Not found"}
+    return jsonify(response), 404
 
 
 if __name__ == "__main__":
