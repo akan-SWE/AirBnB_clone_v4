a4aad99963726602e04be9cf5234c243ad8e7a45
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 08:57:58 +0100

    Add initial aoi

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,4 +11,4 @@ app.register_blueprint(app_views)
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv())
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """"Create flask app"""
+from flask import jsonify
 from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Return the status"""
+    response = {'status': "OK"}
+    return jsonify(response)
