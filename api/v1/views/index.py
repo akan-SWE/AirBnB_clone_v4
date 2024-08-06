a5992bf3012f4fcdc101917956fa8d3e24641525
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 09:45:39 +0100

    Creating additional route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """"Create flask app"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -9,3 +10,16 @@ def status():
     """Return the status"""
     response = {'status': "OK"}
     return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Payload to return db tables"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")  
+    }
+    return jsonify(stats)
