1f625bc9eb845689477f3bb78dd470f98d8a466e
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 09:31:34 +0100

    Added few lines to the app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,17 @@ from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Close storage session"""
+    storage.close()
+
 if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv())
+    HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=HOST, port=PORT, threaded=True)
