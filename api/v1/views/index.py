d14354ca8cbbced1f99f2060fd7d78da62fdaac9
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 09:13:34 +0100

    Trying to make file executable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return the status"""
     response = {'status': "OK"}
