2209a01128759692b4a304dfaef6cfb1ac0bb146
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-06 19:03:39 +0100

    Just added

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -51,7 +51,7 @@ def create_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Update state"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if not state:
         abort(404)
     data = request.get_json()
