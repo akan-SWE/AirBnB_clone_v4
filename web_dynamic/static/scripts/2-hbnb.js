75ac07bef2111cf5cdbca1fdd933eed376520f0d
akanSWE <a.akanSWE@gamil.com>
2024-08-12 01:50:40 +0100

    Update header to reflect API status in div#api_status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,24 +20,23 @@ $(function () {
     }
     $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
   });
-  // Function to check API status
-  function updateApiStatus() {
-    $.ajax({
-      url: 'http://0.0.0.0:5001/api/v1/status/',
-      type: 'GET',
-      dataType: 'json',
-      success: function (json) {
-        if (json.status === 'OK') {
-          $('#api_status').addClass('available');
-       } else {
+
+  // check API status
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    dataType: 'json',
+    success: function (json) {
+      if (json.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
         $('#api_status').removeClass('available');
-        }  
-         },
-      error: function (xhr, status) {
-         // On error, remove the available class
-         $('#api_status').removeClass('available');
-         console.log('Error ' + status);
-     }
- });
-}
+      }
+    },
+    error: function (xhr, status) {
+      // On error, remove the available class
+      $('#api_status').removeClass('available');
+      console.log('Error ' + status);
+    }
+  });
 });
